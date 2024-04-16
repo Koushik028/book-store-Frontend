041bfe10494a11cc76470565002fb7d8453832fb
koushik P <103559026+Koushik028@users.noreply.github.com>
2024-04-16 20:36:08 +0530

    Update UploadBook.jsx

--- src/dashboard/UploadBook.jsx
+++ src/dashboard/UploadBook.jsx
@@ -48,7 +48,7 @@ const UploadBook = () => {
 
     //send data to db
     
-    fetch("https://book-store-backend-kwf7.onrender.com/admin/dashboard/upload-book", {
+    fetch("https://book-store-backend-kwf7.onrender.com/upload-book", {
       method: "POST",
       headers: {
         "Content-Type" : "application/json",
